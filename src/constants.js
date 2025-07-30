b7c7efadc17586a63916d0a1d01b67f85a6e5f91
KishanBaranwal01 <kishanbaranwal7521@gmail.com>
2025-07-30 16:18:22 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,311 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+
+import techhelperlogo from './assets/company_logo/techhelper-logo.png' ;
+import Shubhaminfologo from './assets/company_logo/shubham-info.png'
+
+// Education Section Logo's
+import lnctlogo from './assets/education_logo/Lnct-logo.jpg'
+import omlogo from './assets/education_logo/Om.jpg'
+import giclogo from './assets/education_logo/Gic.jpg'
+import uplogo from './assets/education_logo/up-logo.jpg'
+
+// Project Section Logo's
+
+import jobconnect from './assets/work_logo/jobConnect.png'
+import sehatful from './assets/work_logo/sehatful.png'
+import password from './assets/work_logo/password.png'
+import parallex from './assets/work_logo/parallex.png'
+
+import testybite from './assets/work_logo/TestyBite.png'
+
+import Aichatbot from './assets/work_logo/AichatBot.png'
+import donatebox from './assets/work_logo/donatebBox.png'
+import weather from './assets/work_logo/weather.png';
+import Myntra from './assets/work_logo/myntra.png';
+
+
+
+
+
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      // { name: 'Angular', logo: angularLogo },
+      { name: 'Redux', logo: reduxLogo },
+      // { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      // { name: 'GSAP', logo: gsapLogo },
+      { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      // { name: 'Springboot', logo: springbootLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      // { name: 'Firebase', logo: firebaseLogo },
+      // { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      // { name: 'C-Sharp', logo: csharpLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      // { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: techhelperlogo,
+      role: "MERNStack Developer",
+      company: "TechHelper Technology",
+      date: "April 2025 - Present",
+      desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React JS",
+        "Node JS",
+        "Tailwind CSS",
+        "Bootstrap",
+        "MongoDb",
+        "Redux",
+       
+      ],
+    },
+    // {
+    //   id: 1,
+    //   img: agcLogo,
+    //   role: "Fullstack Engineer",
+    //   company: "Agumentik Group of Companies",
+    //   date: "July 2023 - March 2024",
+    //   desc: "Contributed to innovative projects as a Fullstack Engineer, leading both frontend and backend development using technologies such as HTML, CSS, JavaScript, PHP, SQL, Bootstrap, and ReactJS. Worked closely with the team to deliver responsive, high-performance web applications and improve user experience through seamless integration of various technologies.",
+    //   skills: [
+    //     "ReactJS",
+    //     "Redux",
+    //     "JavaScript",
+    //     "Tailwind CSS",
+    //     "HTML",
+    //     "CSS",
+    //     "SQL",
+    //   ],
+    // },
+    {
+      id: 1,
+      img: Shubhaminfologo,
+      role: "Frontend Intern",
+      company: "Shubham Infotech ",
+      date: "September 2024 - November 2024",
+      desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
+      skills: [
+        "HTML",
+        "CSS",
+        "Javascript",
+        "Tailwind CSS",
+        "Bootstrap",
+        "Figma",
+        "Material UI",
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: lnctlogo,
+      school: "LNCT UNIVERSITY , BHOPAL , MP",
+      date: "August 2023 - July 2025",
+      grade: "8.43 CGPA",
+      desc: "I have completed my Master's degree (MCA) in Computer Applications from LNCT UNIVERSITY , BHOPAL . During my time at LNCT , I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at LNCT University has been instrumental in shaping my technical abilities and professional growth.",
+      degree: "Master of Computer Applications - (MCA - 2025)",
+    },
+    {
+      id: 1,
+      img: omlogo,
+      school: "OM UCHCHA SHIKSHA SANSTHAN , GOPIGANJ , BHADOHI , UP",
+      date: "July 2019 - December 2022",
+      grade: "73.2%",
+      desc: "I completed my BCA from OM Uchcha Shiksha Sansthan, Bhadohi, where I gained a solid foundation in computer science. I studied key subjects like Data Structures, Algorithms, Web Development, and DBMS, and worked on practical projects that applied theoretical concepts to real-world problems. This experience sharpened my technical skills and fueled my passion for software development.",
+      degree: "Bachelor of Computer Application - (BCA-2022)",
+    },
+    {
+      id: 2,
+      img: giclogo,
+      school: "VN GOV INTER COLLEGE , GYANPUR , BHADOHI , UP ",
+      date: "Apr 2017 - March 2018",
+      grade: "68.2%",
+      desc: "I completed my Class 12 from VN Gov. Inter College, Gyanpur, under the State Board, with Physics, Chemistry, and Mathematics (PCM) as core subjects. This phase helped me build a strong foundation in PCM concepts, while maintaining a disciplined and obedient approach toward academics.",
+      degree: "State Board (XII) - PCM with Hindi & English",
+    },
+    {
+      id: 3,
+      img:uplogo,
+      school: "DJMBSS LUXMANPATTI , BHADOHI , UP ",
+      date: "Apr 2015 - March 2016",
+      grade: "84.5 %",
+      desc: "I completed my Class 10 from DJMBSS Luxmanpatti, Bhadohi, under the State Board, with a focus on Science and Hindi. During this time, I maintained a disciplined and obedient attitude toward studies, which helped build a strong academic foundation.",
+      degree: "State Board(X), Science with Hindi",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Job Connector",
+      description:
+        " Job Connector is a MERN stack-based web application where recruiters can register, post, and manage job listings, while job seekers can create profiles, search, and apply for jobs. The project is built using React.js and Tailwind CSS for the frontend, Node.js and Express.js for the backend, and MongoDB as the database. It uses RESTful APIs for client-server communication and JWT for secure authentication.",
+      image: jobconnect,
+      tags: [ "React JS","Tailwind Css" ,"Rest API","Node Js" , "Express JS" ,"MongoDb"],
+      github: "https://github.com/KishanBaranwal01/JobSeekr",
+      webapp: "#",
+    },
+    {
+      id: 1,
+      title: "DonateBox",
+      description:
+        "DonateBox is a responsive and user-friendly blood donation platform designed to bridge the gap between blood donors and recipients. This platform simplifies the process of finding and requesting blood by connecting registered donors with individuals or hospitals in need.",
+      image: donatebox,
+      tags: ["React JS", "Node.js", "MongoDB", "Express", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/KishanBaranwal01/DonateBox",
+      webapp: "https://donate-box-olive.vercel.app/",
+    },
+    {
+      id: 2,
+      title: "AI ChatBot",
+      description:
+        "AI Chatbot is a simple web-based project that allows users to chat with an AI assistant. The chatbot takes user input, sends it to an AI API (like OpenAI), and displays the smart reply on the screen. It’s built using basic web technologies for an interactive and clean user interface.",
+      image: Aichatbot,
+      tags: [  "HTML", "CSS", "JavaScript","API"],
+      github: "https://github.com/KishanBaranwal01/AI-ChatBot",
+      webapp: "https://celebrated-brigadeiros-561138.netlify.app/",
+    },
+    {
+      id: 3,
+      title: "Password Generator ",
+      description:
+        "The Password Generator is a simple web app that creates strong and secure passwords based on user-selected criteria like length, numbers, symbols, and uppercase letters. It helps users generate random passwords instantly to improve online security.",
+      image: password,
+      tags: ["React JS", "Node.js", "NPM", "Validation"],
+      github: "https://github.com/KishanBaranwal01/Password-Generator",
+      webapp: "https://preeminent-travesseiro-544e5f.netlify.app/",
+    },
+    {
+      id: 4,
+      title: "TestyBite",
+      description:
+        "TestyBite is a modern and responsive frontend web application for a food ordering platform. It offers users a smooth experience to browse restaurants, explore food items, add them to the cart, and proceed to checkout.Designed with a clean UI using Tailwind CSS, built using React.js for fast and interactive components, and managed with Redux for global state like cart and user data.",
+      image: testybite,
+      tags: ["JavaScript", "react js ", "redux", "Tailwind CSS"],
+      github: "https://github.com/KishanBaranwal01/FoodOrder",
+      webapp: "https://food-delivery-opal-six.vercel.app/",
+    },
+    {
+      id: 5,
+      title: "Weather Application",
+      description:
+        "A simple and responsive web app built with HTML, CSS, and JavaScript. It shows real-time weather information like temperature, humidity, and wind speed. The app uses the Geolocation API to fetch the user's current location and displays local weather using the OpenWeatherMap API. || Tools Used: HTML, CSS, JavaScript, Geolocation API, OpenWeatherMap API",
+      image: weather,
+      tags: ["HTML", "CSS", "JavaScript", "Geolocation API"],
+      github: "https://github.com/KishanBaranwal01/Weather-Application-",
+      webapp: "https://willowy-clafoutis-6950c4.netlify.app/",
+    },
+    {
+      id: 6,
+      title: "Sehatful.Com",
+      description:
+        "Sehatful.com is a modern and responsive frontend food ordering website focused on delivering healthy meals to customers. The platform allows users to explore nutritious food options, view details, and add items to the cart for a seamless ordering experience.Built using React.js for a fast and interactive UI, and styled with Tailwind CSS for a clean, mobile-friendly design.",
+      image: sehatful,
+      tags: ["React JS","Tailwind Css " ],
+      github: "https://github.com/KishanBaranwal01/Sehatful.com",
+      webapp: "https://food-delivery-fvnw.vercel.app/",
+    },
+    {
+      id: 7,
+      title: "Myntra Clone",
+      description:
+        "The Myntra Clone is a frontend e-commerce website inspired by the original Myntra platform. It is built using HTML, CSS, and JavaScript and replicates the UI/UX of Myntra's homepage, product listings, and product detail pages. The clone includes features like responsive design, product filtering, and a basic cart system (optional). It focuses on delivering a smooth user experience with attractive visuals and structured layout.|| Tools Used: HTML, CSS, JavaScript",
+      image: Myntra,
+      tags: ["HTML", "CSS", "Javascript"],
+      github: "https://github.com/KishanBaranwal01/Myntra-Clone-",
+      webapp: "https://imagsearch.netlify.app/",
+    },
+    {
+      id: 7,
+      title: "Parallex website",
+      description:
+        "This Parallax Website is a visually engaging single-page layout that creates a 3D scrolling effect by moving background images at a slower speed than the foreground content. It enhances user experience with smooth animations and modern design.",
+      image: parallex,
+      tags: ["HTML", "CSS", "JavaScript", "Framer Motion"],
+      github: "https://github.com/KishanBaranwal01/Parallex-website",
+      webapp: "https://bespoke-sable-fff5b9.netlify.app/",
+    },
+  ];  
